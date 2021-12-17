5693e58cada908a2e4530d5188560864026a73c4
carina9231 <carina9872@gmail.com>
2021-12-17 19:02:43 +0900

    12/17 timetest

---
+++ src/main/java/com/sparta/weeklytestspring/security/WebSecurityConfig.java
@@ -0,0 +1,28 @@
+package com.sparta.weeklytestspring.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+    @Configuration
+    @EnableWebSecurity // 스프링 Security 지원을 가능하게 함
+    public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.csrf().disable();
+            http.headers().frameOptions().disable();
+
+            http.authorizeRequests()
+                    .loginPage("/user/login")
+                    .failureUrl("/user/login/error")
+                    .anyRequest().authenticated()
+                    .and()
+                    .formLogin()
+                    .defaultSuccessUrl("/")
+                    .permitAll()
+                    .and()
+                    .logout()
+                    .permitAll();
+        }
+    }
